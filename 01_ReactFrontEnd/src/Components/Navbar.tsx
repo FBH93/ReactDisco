0d5a1540e0dc370f279be9af36dac1c1242fb49f
Frederik Bøye Henriksen <Frederik@boycom.dk>
2022-05-19 14:27:40 +0200

    added event listeners to navbar

--- 01_ReactFrontEnd/src/Components/Navbar.tsx
+++ 01_ReactFrontEnd/src/Components/Navbar.tsx
@@ -3,41 +3,48 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import React, {useCallback, useRef} from 'react'
+import React, {Component, useCallback, useRef, useState} from 'react'
 
-export const NavigationBar = () => (
-<Navbar bg="dark" variant="dark" expand="lg">
-    <Container>
-        <Navbar.Brand href="#home">
-        <img
-            src="assets/img/logo/logo-discoclothing--white.svg"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-        />
-    </Navbar.Brand>
-  </Container>
-    <Container>
-        <Navbar.Brand href="#home">Home</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Some Button</Nav.Link>
-                    <Nav.Link href="#link">All Products</Nav.Link>
-                    <NavDropdown title="Filters" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Style</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Type</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Price</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Some separate link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav>
-                    <Nav.Link href="#profile">Profile</Nav.Link>
-                    <Nav.Link eventKey={2} href="#basket">basket</Nav.Link>
-                </Nav>
-        </Navbar.Collapse>
+export function NavigationBar() {
+    const [value,setValue]=useState('');
+    const handleSelect=(e)=>{
+      console.log(e);
+      setValue(e)
+    }
+    return (
+    <Navbar bg="dark" variant="dark" expand="lg" onSelect={handleSelect}>
+        <Container>
+            <Navbar.Brand href="#home" >
+            <img
+                src="assets/img/logo/logo-discoclothing--white.svg"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+            />
+        </Navbar.Brand>
     </Container>
-</Navbar>
-)
+        <Container>
+            <Navbar.Brand href="#home">Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link eventKey="button" href="#button">Some Button</Nav.Link>
+                        <Nav.Link eventKey="allProducts" href="#products">All Products</Nav.Link>
+                        <NavDropdown title="Filters" id="basic-nav-dropdown">
+                            <NavDropdown.Item eventKey="Styles" href="#style">Style</NavDropdown.Item>
+                            <NavDropdown.Item eventKey="Types" href="#Type">Type</NavDropdown.Item>
+                            <NavDropdown.Item eventKey="Prices" href="#Price">Price</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item eventKey="SomeLink" href="#SomeLink">Some separate link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link eventKey="Profile" href="#profile">Profile</Nav.Link>
+                        <Nav.Link eventKey={"Basket"} href="#basket">basket</Nav.Link>
+                    </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    )
+}
 export default Navbar
