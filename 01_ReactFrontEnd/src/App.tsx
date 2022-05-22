5d305b8f535e0882852c323a1ff18fe8ec04e073
Roman Heubel <rheu@itu.dk>
2022-05-22 16:33:27 +0200

    Navigation Issues

--- 01_ReactFrontEnd/src/App.tsx
+++ 01_ReactFrontEnd/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import { NavigationBar } from './Components/Navbar';
 import Background from './Components/Background';
 import ProductGrid from './Components/ProductGrid';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import { Product } from './Components/Product';
 import { render } from 'react-dom';
 
@@ -16,12 +16,12 @@ class App extends React.Component {
         <div>
           <NavigationBar />
           <Routes>
-              <Route path="/" element={<ProductGrid filter1={'70s'} filter2={'None'}/>}/>
-              <Route path="/products/70s" element={<ProductGrid filter1={'70s'} filter2={'None'}/>} />
-              <Route path="/products/80s" element={<ProductGrid filter1={'80s'} filter2={'None'}/>} />
-              <Route path="/products/sportswear" element={<ProductGrid filter1={'sportswear'} filter2={'None'}/>} />
-              <Route path="/products/space" element={<ProductGrid filter1={'space'} filter2={'None'}/>} />
-              <Route path="/product/:id" element={<Product id={1}/>}/>
+              <Route path="/" element={<ProductGrid filter1={'None'} filter2={'None'}/>}/>
+              <Route path="/products/70s" element={<ProductGrid filter1={'style=70s'} filter2={'None'}/>} />
+              <Route path="/products/80s" element={<ProductGrid filter1={'style=80s'} filter2={'None'}/>} />
+              <Route path="/products/sportswear" element={<ProductGrid filter1={'style=sportswear'} filter2={'None'}/>} />
+              <Route path="/products/space" element={<ProductGrid filter1={'style=space'} filter2={'None'}/>} />
+              <Route path="/product/:id" element={<Product />}/>
           </Routes>
         </div>
       </BrowserRouter>
