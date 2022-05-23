73fe693786c4299687c44431e72e9dac1a8cead2
Maria Andersen <maris@itu.dk>
2022-05-23 20:39:03 +0200

    fetch put

--- 01_ReactFrontEnd/src/Services/UserCall.tsx
+++ 01_ReactFrontEnd/src/Services/UserCall.tsx
@@ -2,14 +2,21 @@ import { useState, useEffect } from 'react'
 import { NumericLiteral } from 'typescript'
 
 //get products from API
-function getUserFromAPI(email:string){
-   return 'http://localhost:3000/customer/' + email
+function getUserFromAPI(email: string) {
+  return 'http://localhost:3000/customer/' + email
 }
 
-export function putProductToBasket(userID:string, pID:number, size:string)  {
-  return fetch('http://localhost:3000/basket/' + userID + '/?productID=' + pID +'&size=' + size)
-        .then((res) => res.json())
-        .then((res) => {
-          console.log(res)
-        })
-};
+export function putProductToBasket(userID: string, pID: number, size: string) {
+  return fetch(
+    'http://localhost:3000/basket/' +
+      userID +
+      '/?productID=' +
+      pID +
+      '&size=' +
+      size
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res)
+    })
+}
