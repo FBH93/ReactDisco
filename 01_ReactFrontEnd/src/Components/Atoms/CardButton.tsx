2f431d7e656b3a31fbf6d0b1991d9d3365ff38c0
Maria Andersen <maris@itu.dk>
2022-05-23 15:49:53 +0200

    button update

--- 01_ReactFrontEnd/src/Components/Atoms/CardButton.tsx
+++ 01_ReactFrontEnd/src/Components/Atoms/CardButton.tsx
@@ -1,4 +1,4 @@
-import { Component, useState } from 'react'
+import React, { Component, useState } from 'react'
 import { Button } from 'react-bootstrap'
 import Alert from 'react-bootstrap/Alert'
 import { addSyntheticTrailingComment } from 'typescript'
@@ -8,26 +8,22 @@ import { Basket } from '../Basket'
 export const CardButton = () => {
 
   const [selectedSize, setSelectedSize] = useState("");
-  const [isAdded, setIsAdded] = useState(false);
-  const id = 1;
-
-  const buttonClick = () => {
-    //addItem(id)
-    setIsAdded(true)
-  };
 
   const addItem = (id:number) => { 
     localStorage.setItem("product" + id, "product, " + selectedSize)
   };
 
-
-    return (
-      <>
+      return (
+        <>
           <div className="col">
-            <Button onClick = {buttonClick} className="btn btn-primary" data-bss-hover-animate="pulse" type="button" style={{ marginBottom: '25px' }}>Add to cart</Button></div>
-           {isAdded ? <Alert variant="success"> Added to cart! </Alert> : null }
-      </>
-    )
+            <button
+              onClick = {(event) => {
+                addItem(1)
+                alert("Added to basket")
+              }} className="btn btn-primary" data-bss-hover-animate="pulse" type="button" style={{ marginBottom: '25px' }}>Add to cart</button>
+          </div>
+        </>
+      )
 }
 
 
