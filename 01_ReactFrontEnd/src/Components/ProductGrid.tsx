905ac9fbc51ada312dd877cb86774dc867365060
Frederik Bøye Henriksen <Frederik@boycom.dk>
2022-05-25 09:42:26 +0200

    productGrid card

--- 01_ReactFrontEnd/src/Components/ProductGrid.tsx
+++ 01_ReactFrontEnd/src/Components/ProductGrid.tsx
@@ -1,11 +1,11 @@
-import { ProductInterface } from '../Components/Product'
-import Sizometer from './Atoms/Sizometer'
-import { useState, useEffect } from 'react'
-import { CardButton } from './Atoms/CardButton'
-import { Alert, Card } from 'react-bootstrap'
-import { getAPI,  } from '../Services/ProductsCall'
-import { useAtom } from 'jotai'
-import { sizeAtom } from './store'
+import { ProductInterface } from "../Components/Product"
+import Sizometer from "./Atoms/Sizometer"
+import { useState, useEffect } from "react"
+import { CardButton } from "./Atoms/CardButton"
+import { Alert, Card } from "react-bootstrap"
+import { getAPI } from "../Services/ProductsCall"
+import { useAtom } from "jotai"
+import { sizeAtom } from "./store"
 
 export type Filter = {
   filter1: string
@@ -19,9 +19,9 @@ function ProductGrid(filter: Filter, props) {
   const API = getAPI(filter)
   console.log(API)
 
-  useEffect(() =>{
+  useEffect(() => {
     const fetchProducts = async () => {
-      fetch(API,{method: 'GET'})
+      fetch(API, { method: "GET" })
         .then((res) => res.json())
         .then((res) => {
           console.log(res) //This prints twice, for some reason??
@@ -33,37 +33,42 @@ function ProductGrid(filter: Filter, props) {
 
   if (products) {
     return (
-      <div className="container discoGrid justify-content-center d-flex flex-wrap mt-5">
-        <>
-          {products.map((product, i) => {
-            return (
-              <Card 
-                className='text-center'
-                bg='light'
-                style={{ width: '18rem', margin: '5px' }}
-              >
-                <a
-                  className="shopGridLink"
-                  href={'/products/' + product.productID}
-                  style={{ color: '#455f58' }}
+      <Card bg="light" border="dark">
+          <div className="container discoGrid justify-content-center d-flex flex-wrap mt-5">
+            {products.map((product, i) => {
+              return (
+                <Card
+                  className="text-center"
+                  bg="light"
+                  border="light"
+                  style={{ width: "18rem", margin: "5px" }}
                 >
-                  <Card.Img variant="top" src={'/assets/img/products/' + product.productID + '.jpg'} />
-                  <Card.Title>{product.productName}</Card.Title>
-                  <Card.Subtitle>{product.productPrice + ' DKK'}</Card.Subtitle>
-                </a>
-                <CardButton pID={product.productID} size={selectedSize} />
-                <Sizometer />
-              </Card>
-            )
-          })}
-        </>
-      </div>
+                  <a
+                    className="shopGridLink"
+                    href={"/products/" + product.productID}
+                    style={{ color: "#455f58" }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={"/assets/img/products/" + product.productID + ".jpg"}
+                    />
+                    <Card.Title>{product.productName}</Card.Title>
+                    <Card.Subtitle>
+                      {product.productPrice + " DKK"}
+                    </Card.Subtitle>
+                  </a>
+                  <CardButton pID={product.productID} size={selectedSize} />
+                  <Sizometer />
+                </Card>
+              )
+            })}
+          </div>
+      </Card>
     )
   } else return <div> FAULTY FILTER </div>
 }
 
 export default ProductGrid
-
 
 /*
  <img
