09589bc4b3770b148d3255e8f8d0b2537f1f2cd2
Roman Heubel <rheu@itu.dk>
2022-05-26 22:56:27 +0200

    CustomerID Fix (hopefully)

--- 00_ExpressAPI/routes/customer/customer.route.js
+++ 00_ExpressAPI/routes/customer/customer.route.js
@@ -1,12 +1,17 @@
-import express from "express";
-import { getCustomerById, postCustomer, getAllCustomers, getCustomerByEmail } from "./customer.controler.js";
+import express from "express"
+import {
+  getCustomerById,
+  postCustomer,
+  getAllCustomers,
+  getCustomerByEmail,
+} from "./customer.controler.js"
 
-export const customerRouter = express.Router();
+export const customerRouter = express.Router()
 
 //Middelware
-customerRouter.use(express.json());
+customerRouter.use(express.json())
 
-customerRouter.get("/customer/:customerId", getCustomerById);
-customerRouter.post("/customer/", postCustomer);
-customerRouter.get("/customer/", getAllCustomers);
-customerRouter.get("/customer/login/:email", getCustomerByEmail);
+customerRouter.get("/customer/:customerID", getCustomerById)
+customerRouter.post("/customer/", postCustomer)
+customerRouter.get("/customer/", getAllCustomers)
+customerRouter.get("/customer/login/:email", getCustomerByEmail)
