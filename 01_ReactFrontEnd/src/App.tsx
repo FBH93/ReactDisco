6dfbc34b7f91ede7125995c292118c1ec5030f99
romanheubel <55709231+romanheubel@users.noreply.github.com>
2022-05-26 15:27:56 +0200

    Animate CSS

--- 01_ReactFrontEnd/src/App.tsx
+++ 01_ReactFrontEnd/src/App.tsx
@@ -1,13 +1,12 @@
-import '../src/custom.scss'
-import Animate from 'animate.css-react'
-import 'animate.css/animate.css'
-import React from 'react'
-import { NavigationBar } from './Components/Navbar'
-import { Basket } from './Components/Basket'
-import ProductGrid from './Components/ProductGrid'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Product from './Components/Product'
-import { Home } from './Components/Home'
+import "../src/custom.scss"
+import "animate.css/animate.css"
+import React from "react"
+import { NavigationBar } from "./Components/Navbar"
+import { Basket } from "./Components/Basket"
+import ProductGrid from "./Components/ProductGrid"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Product from "./Components/Product"
+import { Home } from "./Components/Home"
 
 class App extends React.Component {
   render() {
@@ -27,82 +26,66 @@ class App extends React.Component {
               />
               <Route
                 path="/products"
-                element={
-                  <ProductGrid filter1={'None'} filter2={'None'} />
-                }
+                element={<ProductGrid filter1={"None"} filter2={"None"} />}
               />
               <Route
                 path="/products/70s"
-                element={
-                  <ProductGrid filter1={'style=70s'} filter2={'None'} />
-                }
+                element={<ProductGrid filter1={"style=70s"} filter2={"None"} />}
               />
               <Route
                 path="/products/80s"
-                element={
-                  <ProductGrid filter1={'style=80s'} filter2={'None'} />
-                }
+                element={<ProductGrid filter1={"style=80s"} filter2={"None"} />}
               />
               <Route
                 path="/products/sportswear"
                 element={
-                  <ProductGrid filter1={'style=sportswear'} filter2={'None'} />
+                  <ProductGrid filter1={"style=sportswear"} filter2={"None"} />
                 }
               />
               <Route
                 path="/products/space"
                 element={
-                  <ProductGrid filter1={'style=space'} filter2={'None'} />
+                  <ProductGrid filter1={"style=space"} filter2={"None"} />
                 }
               />
               <Route
                 path="/products/shirts"
                 element={
-                  <ProductGrid filter1={'type=shirt'} filter2={'None'} />
+                  <ProductGrid filter1={"type=shirt"} filter2={"None"} />
                 }
               />
               <Route
                 path="/products/pants"
                 element={
-                  <ProductGrid filter1={'type=pants'} filter2={'None'} />
+                  <ProductGrid filter1={"type=pants"} filter2={"None"} />
                 }
               />
               <Route
                 path="/products/jackets"
                 element={
-                  <ProductGrid filter1={'type=jackets'} filter2={'None'} />
+                  <ProductGrid filter1={"type=jackets"} filter2={"None"} />
                 }
               />
               <Route
                 path="/products/accessories"
                 element={
-                  <ProductGrid filter1={'type=accessories'} filter2={'None'} />
+                  <ProductGrid filter1={"type=accessories"} filter2={"None"} />
                 }
               />
               <Route
                 path="/products/premium"
                 element={
-                  <ProductGrid filter1={'price=premium'} filter2={'None'} />
+                  <ProductGrid filter1={"price=premium"} filter2={"None"} />
                 }
               />
               <Route
                 path="/products/discount"
                 element={
-                  <ProductGrid filter1={'price=discount'} filter2={'None'} />
+                  <ProductGrid filter1={"price=discount"} filter2={"None"} />
                 }
               />
-              <Route 
-                path="/products/:id" 
-                element={
-                  <Product />
-                } 
-              />
-              <Route 
-                path="/basket" 
-                element={
-                  <Basket />
-                } 
-              />
+              <Route path="/products/:id" element={<Product />} />
+              <Route path="/basket" element={<Basket />} />
             </Routes>
           </div>
         </BrowserRouter>
