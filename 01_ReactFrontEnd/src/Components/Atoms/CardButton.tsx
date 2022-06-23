6fcb3bdf20edcae17d34fe85c75bb5e7d4ae535c
Maria Andersen <maris@itu.dk>
2022-06-23 15:48:53 +0200

    comments for product and carbutton

--- 01_ReactFrontEnd/src/Components/Atoms/CardButton.tsx
+++ 01_ReactFrontEnd/src/Components/Atoms/CardButton.tsx
@@ -4,18 +4,22 @@ import { useAtom } from "jotai"
 import { cardAlertAtom } from "../store"
 import { CardAlert } from "./CardAlert"
 
+// creating custom type CardButtonProps with type properties productID of number and size of string
 type CardButtonProps = {
   pID: number
   size: string
 }
 
+// in the same way as if we were declaring the type of this function to expect a number or string, here we are using type annotation to show that the type is CardButtonProps, which expects a productID and size.
 export const CardButton: React.FC<CardButtonProps> = ({ pID, size }) => {
-  const [, setAlert] = useAtom(cardAlertAtom)
+  const [, setAlert] = useAtom(cardAlertAtom) // used to alert a message to the user when a product is added to the cart. We use useAtom instead of useState as it then can be used anywhere in the code even if it is a child component on the same or lower level in the hierarchy. It doesn't need to be on the parent level before it can be used, as it is an atom (making it accessible from anywhere).
   const addItem = (pID: number, size: string) => {
     if (localStorage.getItem("customerID")) {
+      // getting the customerID from localstorage.
       let cID = localStorage.getItem("customerID")
       if (cID) {
-        putProductToBasket(cID, pID, size)
+        // checking if a customerID exists - checking if the user is registered and logged in
+        putProductToBasket(cID, pID, size) // if the user is logged in a PUT request from the UserCall.tsx is called which needs customerID, productID and selectde size.
       }
     } else {
       localStorage.setItem("product" + pID, "product, " + size)
@@ -30,7 +34,7 @@ export const CardButton: React.FC<CardButtonProps> = ({ pID, size }) => {
           className="my-3"
           onClick={(event) => {
             addItem(pID, size)
-            setAlert(true)
+            setAlert(true) // when the "add to cart" button is clicked it will trigger an event that uses productID and selected size (default S) to call the addItem function and sets the useAtom to true meaning that an alert will pop up letting the user know that the product has been added to the cart.
           }}
         >
           Add to cart
--- 01_ReactFrontEnd/src/Components/Templates/Product.tsx
+++ 01_ReactFrontEnd/src/Components/Templates/Product.tsx
@@ -6,6 +6,8 @@ import { CardButton } from "../Atoms/CardButton"
 import Sizometer from "../Atoms/Sizometer"
 import { sizeAtom } from "../store"
 
+// an interface is a type without implementation
+// here we are creating an interface for product to declare the certain type of structure that a product has. This means that when using the ProductInterface the constant has to comply with all elements of ProductInterface, meaning it has to match the structure of the interface. If we get something that does not have structure as below, then it is not what we define as a product.
 export interface ProductInterface {
   productID: number
   productName: string
@@ -16,18 +18,20 @@ export interface ProductInterface {
 }
 
 const Product = () => {
-  let { id } = useParams()
+  let { id } = useParams() //taking the parameter from the URL so here if productID in the URL is 2 then { id } is 2
   const [selectedSize] = useAtom(sizeAtom)
 
+  // Has to match the structure of the interface. Starting state is null. This will be updated to a product when we get a prodyctID.
   const [myProduct, setMyProduct] = useState<ProductInterface | null>(null)
 
   useEffect(() => {
     const updateProduct = async () => {
-      const myProduct = await getSingleProduct(id)
-      setMyProduct(myProduct)
+      //expecting a promise
+      const myProduct = await getSingleProduct(id) // awaiting the promise resolved by the getSingleProduct method
+      setMyProduct(myProduct) //myProduct now exists of the json data of ONE product with id asked for
     }
     updateProduct()
-  }, [id])
+  }, [id]) //useEffect is a Hook that the React remembers the function and executes after the render which here shows the product with id asked for above
 
   if (!id) {
     return <>Error</>
@@ -49,7 +53,8 @@ const Product = () => {
             />
           </div>
           <div className="col mt-5">
-            <h2 id="discoProductName">{myProduct?.productName}</h2>
+            <h2 id="discoProductName">{myProduct?.productName}</h2>{" "}
+            {/* the ? will return and undefined if myProduct is null */}
             <div className="row">
               <div className="col">
                 <p> {myProduct?.details} </p>
@@ -64,7 +69,8 @@ const Product = () => {
             </div>
             <div className="row">
               <div className="col">
-                <CardButton pID={parseInt(id)} size={selectedSize} />
+                <CardButton pID={parseInt(id)} size={selectedSize} />{" "}
+                {/** sizeometer and cardbutton could have been combined as a molecule because we never use them individually */}
               </div>
             </div>
           </div>
